94e4fb8f4af3a21939709a0f3de5a4e8c7a1fb69
Njeri <njeri.gachago@gmail.com>
2021-01-22 18:55:53 +0900

    modified styles and fixed page links

--- helper.py
+++ helper.py
@@ -4,9 +4,8 @@ import os
 
 def return_db_row_for_content_type(content_type):
     '''
-    Function to check language and choose correct table in database
+    Function to choose correct table in database
     Args: content_type = str; the content that should be queried from the databases
-          lang = str; language
     Returns: df_row = contains results from sql query
     '''
 
@@ -39,7 +38,6 @@ def get_title_content(page):
     '''
     Function to get the relevant content for the html page
     Args: page = str; name of the html page
-          lang = str; specifies language selected by user
     Returns: title_text = str; content for the html page
     '''
 
@@ -59,7 +57,6 @@ def get_title_content(page):
 def get_portfolio_content():
     '''
     Function to get the portfolio projects from the database
-    Args: lang = str; specifies language selected by user
     Returns: zipped
     '''
 
@@ -97,7 +94,6 @@ def get_portfolio_content():
 def get_skill_content():
     '''
     Function to get the skills from the database
-    Args: lang = str; specifies language selected by user
     Returns: skill_list
     '''
 
